fa181aede5b97726ed7aee9f414be630344f3590
Iris Ward <aditu.venyhandottir@gmail.com>
2017-10-14 16:04:37 +0100

    Fixed test preprocess

--- Setup.hs
+++ Setup.hs
@@ -1,10 +1,12 @@
 import Distribution.Simple
 import Distribution.Simple.PreProcess
 
+import System.FilePath(takeBaseName)
 
 main = defaultMainWithHooks simpleUserHooks {
            hookedPreProcessors = [ ("proto", ppProto) ]
          }
+  where
 
     -- Dummy implementation: creates a module with a string containing
     -- the module's original filename
@@ -12,16 +14,15 @@ main = defaultMainWithHooks simpleUserHooks {
         PreProcessor 
           { platformIndependent = True
           , runPreProcessor = mkSimplePreProcessor $ \inFile outFile verbosity ->
-              writeFile outFile (testPreprocess inFile)
+              do
+                let fileContents = testPreprocess inFile
+                writeFile outFile (testPreprocess inFile)
           }
       where
-
         testPreprocess fileName = unlines $
-          [ concat ["module ", stripExtension fileName, " where"]
+          [ concat ["module ", takeBaseName fileName, " where"]
           , ""
           , "someString :: String"
-          , "someString = " ++ fileName
+          , "someString = \"" ++ fileName ++ "\""
           , ""
           ]
-
-        stripExtension = takeWhile (/= '.')
