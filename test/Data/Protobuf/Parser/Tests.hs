029c6dd70e4101f37e24649146b92833d70150d8
Iris Ward <aditu.venyhandottir@gmail.com>
2017-10-26 19:04:58 +0100

    More parser tests
    Further implemented parser tests.
    Fixed bugs this revealed, including sepBy instead of sepBy1 in
    `reservation`.
    
    Also ran code through stylish-haskell

--- test/Data/Protobuf/Parser/Tests.hs
+++ test/Data/Protobuf/Parser/Tests.hs
@@ -1,25 +1,39 @@
-{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
-module Data.Protobuf.Parser.Tests(parserTests) where
+{-# LANGUAGE NamedFieldPuns      #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-import Data.Protobuf.Parser.Internal
-import Data.Protobuf.ProtoSpec
+module Data.Protobuf.Parser.Tests
+  ( parserTests
+  ) where
 
-import Test.Hspec
-import Test.Hspec.Megaparsec
-import Test.QuickCheck
-import Test.QuickCheck.Instances
+import           Data.Protobuf.Parser.Internal
+import           Data.Protobuf.ProtoSpec
 
-import Data.Semigroup((<>))
-import qualified Data.Text as T
-import Text.Megaparsec
+import           Test.Hspec
+import           Test.Hspec.Megaparsec
+import           Test.QuickCheck
+import           Test.QuickCheck.Instances
+
+import           Control.Applicative           (liftA2)
+import           Control.Monad                 (replicateM)
+import           Data.Semigroup                ((<>))
+import qualified Data.Text                     as T
+import           Text.Megaparsec
 
 parserTests :: Spec
-parserTests = describe "Protobuf parser" $ do
-  identifierTests
-  protoTypeTests
+parserTests =
+  describe "Protobuf parser" $ do
+    identifierTests
+    protoTypeTests
+    reservationTests
+    fieldModifierTests
+    fieldSpecTests
+    specBodyEntryTests
+    protoSpecBodyTests
+    protoSpecTests
 
 -- * Test utilities
-
 anyIdentifier :: Gen String
 anyIdentifier = (:) <$> anyAlpha <*> listOf anyIdChar
 
@@ -31,29 +45,29 @@ anyIdChar = frequency [(52, anyAlpha), (10, numeric), (1, pure '_')]
 
 badIdentifier :: Gen String
 badIdentifier = do
-    -- guarantee identifier of length at least 2
-    ident <- anyIdentifier
-    bad <- elements ['!'..'/']
-    pos <- choose (0, length ident - 1)
-    let (start, end) = splitAt pos ident
-    pure (start ++ bad:end)
+  ident <- anyIdentifier
+  bad <- elements ['!' .. '/']
+  pos <- choose (0, length ident - 1)
+  let (start, end) = splitAt pos ident
+  pure (start ++ bad : end)
 
 -- String representations of scalar type names
 typeStrings :: [(ProtoType, String)]
-typeStrings = [ (PDouble, "double")
-              , (PFloat, "float")
-              , (PInt32, "int32")
-              , (PInt64, "int64")
-              , (PUInt32, "uint32")
-              , (PUInt64, "uint64")
-              , (PSInt32, "sint32")
-              , (PSInt64, "sint64")
-              , (PFixed32, "fixed32")
-              , (PFixed64, "fixed64")
-              , (PBool, "bool")
-              , (PString, "string")
-              , (PBytes, "bytes")
-              ]
+typeStrings =
+  [ (PDouble, "double")
+  , (PFloat, "float")
+  , (PInt32, "int32")
+  , (PInt64, "int64")
+  , (PUInt32, "uint32")
+  , (PUInt64, "uint64")
+  , (PSInt32, "sint32")
+  , (PSInt64, "sint64")
+  , (PFixed32, "fixed32")
+  , (PFixed64, "fixed64")
+  , (PBool, "bool")
+  , (PString, "string")
+  , (PBytes, "bytes")
+  ]
 
 scalarTypeString :: Gen (ProtoType, String)
 scalarTypeString = elements typeStrings
@@ -62,39 +76,258 @@ anyTypeString :: Gen (ProtoType, String)
 anyTypeString = oneof [scalarTypeString, nonTSIdent]
   where
     nonTSIdent = do
-      ident' <- anyIdentifier `suchThat`
-                  (\e -> e `notElem` fmap snd typeStrings)
+      ident' <-
+        anyIdentifier `suchThat` (\e -> e `notElem` fmap snd typeStrings
+                              && e /= "reserved" && e /= "message")
       pure (NamedField ident', ident')
 
--- * Tests
+repeatedFieldSpec :: Gen T.Text
+repeatedFieldSpec = do
+  restOfSpec <- optionalFieldSpec
+  pure $ "repeated " <> restOfSpec
 
+optionalFieldSpec :: Gen T.Text
+optionalFieldSpec = do
+  (_, type') <- anyTypeString
+  name <- anyIdentifier
+  tag <- show <$> (positive :: Gen Int)
+  pure . T.pack $ type' <> " " <> name <> " = " <> tag <> ";"
+
+anyFieldSpec :: Gen T.Text
+anyFieldSpec = oneof [repeatedFieldSpec, optionalFieldSpec]
+
+anyResTagEntry :: Gen T.Text
+anyResTagEntry = oneof $ fmap (T.pack) <$> [singleTag, tagRange, tagToMax]
+  where
+    singleTag = show <$> (positive :: Gen Int)
+    tagRange = do
+      t1 <- singleTag
+      t2 <- singleTag
+      pure (t1 <> " to " <> t2)
+    tagToMax = (<> " to max") <$> singleTag
+
+multipleTagEntries :: Int -> Gen (Int, T.Text)
+multipleTagEntries max' = do
+  n <- choose (1, max')
+  entries <- replicateM n anyResTagEntry
+  pure $ (n, T.intercalate ", " entries)
+
+anyResNameEntry :: Gen T.Text
+anyResNameEntry = do
+  n <- anyIdentifier
+  pure . T.pack $ "\"" <> n <> "\""
+
+multipleNameEntries :: Int -> Gen (Int, T.Text)
+multipleNameEntries max' = do
+  n <- choose (1, max')
+  entries <- replicateM n anyResNameEntry
+  pure $ (n, T.intercalate ", " entries)
+
+-- * Tests
 identifierTests :: Spec
-identifierTests = describe "identifier" $ do
-  it "accepts a single-letter identifier" $
-    runParser identifier "" "a" `shouldParse` "a"
-  it "accepts a longer identifier" $
-    runParser identifier "" "foo345bar6" `shouldParse` "foo345bar6"
-  it "accepts randomly-generated identifiers" $ property $
-    forAll anyIdentifier $
-      \t -> runParser identifier "" (T.pack t) `shouldParse` t
-  it "fails to parse integers" $ property $ \(Positive (t :: Int)) -> 
-    runParser identifier "" (T.pack $ show t) `shouldFailWith`
-      err posI ((utok . head . show $ t) <> elabel "identifier")
-  it "fails to parse bad identifiers" $ property $
-    forAll badIdentifier $
-      \s -> runParser (identifier <* eof) "" `shouldFailOn` T.pack s
+identifierTests =
+  describe "identifier" $ do
+    it "parses a single-letter identifier" $
+      runParser identifier "" "a" `shouldParse` "a"
+
+    it "parses a longer identifier" $
+      runParser identifier "" "foo345bar6" `shouldParse` "foo345bar6"
+
+    it "parses randomly-generated identifiers" $
+      property $
+      forAll anyIdentifier $ \t ->
+        runParser identifier "" (T.pack t) `shouldParse` t
+
+    it "fails to parse integers" $
+      property $ \(Positive (t :: Int)) ->
+        runParser identifier "" (T.pack $ show t) `shouldFailWith`
+        err posI ((utok . head . show $ t) <> elabel "identifier")
+
+    it "fails to parse bad identifiers" $
+      property $
+      forAll badIdentifier $ \s ->
+        runParser (identifier <* eof) "" `shouldFailOn` T.pack s
 
 protoTypeTests :: Spec
-protoTypeTests = describe "protoType" $ do
-  it "accepts double" $
-    runParser protoType "" "double" `shouldParse` PDouble
-  it "accepts string" $
-    runParser protoType "" "string" `shouldParse` PString
-  it "accepts an identifier" $ 
-    runParser protoType "" "test3" `shouldParse` (NamedField "test3")
-  it "accepts any scalar type" $ property $ forAll scalarTypeString $
-    \(expected, str) -> runParser protoType "" (T.pack str)
-      `shouldParse` expected
-  it "accepts any valid type string" $ property $ forAll anyTypeString $
-    \(expected, str) -> runParser protoType "" (T.pack str)
-      `shouldParse` expected
+protoTypeTests =
+  describe "protoType" $ do
+    it "parses double" $ runParser protoType "" "double" `shouldParse` PDouble
+
+    it "parses string" $ runParser protoType "" "string" `shouldParse` PString
+
+    it "parses an identifier" $
+      runParser protoType "" "test3" `shouldParse` NamedField "test3"
+
+    it "parses any scalar type" $
+      property $
+      forAll scalarTypeString $ \(expected, str) ->
+        runParser protoType "" (T.pack str) `shouldParse` expected
+
+    it "parses any valid type string" $
+      property $
+      forAll anyTypeString $ \(expected, str) ->
+        runParser protoType "" (T.pack str) `shouldParse` expected
+
+    it "fails to parse bad identifiers" $ property $
+      forAll badIdentifier $ \s ->
+        runParser (protoType <* eof) "" `shouldFailOn` (T.pack s)
+
+reservationTests :: Spec
+reservationTests =
+  describe "reservation" $ do
+    it "parses a single name reservation" $
+      runParser reservation "" "reserved \"blah\";"
+      `shouldParse` [RName "blah"]
+
+    it "parses a single randomly-generated name reservation" $
+      property $
+      forAll anyIdentifier $ \t ->
+        runParser reservation "" ("reserved \"" <> T.pack t <> "\";")
+        `shouldParse` [RName t]
+
+    it "parses a single tag reservation" $
+      runParser reservation "" "reserved 3;" `shouldParse` [RTag 3]
+
+    it "parses a single randomly-generated tag reservation" $
+      property $ \(Positive t) ->
+        runParser reservation "" ("reserved " <> T.pack (show t) <> ";")
+        `shouldParse` [RTag t]
+
+    it "parses a tag range reservation" $
+      runParser reservation "" "reserved 3 to 10;"
+      `shouldParse` [RTagRange 3 10]
+
+    it "parses a randomly-generated tag range reservation" $
+      property $ \(Positive x) (Positive y) ->
+        runParser
+          reservation
+          ""
+          ("reserved " <> T.pack (show x) <> " to " <> T.pack (show y) <> ";")
+        `shouldParse` [RTagRange x y]
+
+    it "parses a tag range to max reservation" $
+      runParser reservation "" "reserved 100 to max;" `shouldParse`
+      [RTagToMax 100]
+
+    it "parses a random tag range to max reservation" $
+      property $ \(Positive t) ->
+        runParser
+          reservation
+          ""
+          ("reserved " <> T.pack (show t) <> " to max;")
+          `shouldParse` [RTagToMax t]
+
+    it "parses multiple tag reservations in one line" $
+      property $ forAll (multipleTagEntries 40) $ \(n,t) ->
+        runParser reservation "" ("reserved " <> t <> ";")
+          `parseSatisfies` \res -> length res == n
+
+    it "parses multiple name reservations in one line" $
+      property $ forAll (multipleNameEntries 40) $ \(n,t) ->
+        runParser reservation "" ("reserved " <> t <> ";")
+          `parseSatisfies` \res -> length res == n
+
+
+    it "fails to parse a bare identifier" $
+      let input = "reserved test" :: T.Text
+      in  runParser reservation "" input `shouldFailWith`
+          err
+            (posN (9 :: Int) input)
+            (utok 't' <> elabel "decimal integer" <>
+             elabel "identifier string")
+
+    it "fails to parse with a missing semicolon" $
+      let input = "reserved 50" :: T.Text
+      in  runParser reservation "" input `shouldFailWith`
+          err
+            (posN (12 :: Int) input)
+            (ueof <> etoks "to" <> etok ',' <> etok ';' <>
+             elabel "the rest of decimal integer")
+
+    it "fails and consumes no input when attempting to parse a fieldspec" $
+      property $ forAll anyFieldSpec $ \spec ->
+        runParser' reservation (initialState spec) `failsLeaving` spec
+
+
+fieldModifierTests :: Spec
+fieldModifierTests =
+  describe "fieldModifier" $ do
+    it "parses \"repeated\"" $
+      runParser fieldModifier "" "repeated" `shouldParse` Repeated
+
+    it "parses Optional when not \"repeated\"" $ property $
+      forAll anyIdentifier $ \t ->
+        t /= "repeated" ==> runParser fieldModifier "" (T.pack t)
+          `shouldParse` Optional
+
+    it "consumes no input for any identifier not \"repeated\"" $ property $
+      forAll anyIdentifier $ \t ->
+        t /= "repeated" ==> runParser' fieldModifier (initialState $ T.pack t)
+          `succeedsLeaving` (T.pack t)
+
+fieldSpecTests :: Spec
+fieldSpecTests =
+  describe "fieldSpec" $ do
+    it "parses any fieldspec with repeated as having Repeated" $ property $
+      forAll repeatedFieldSpec $ \t -> runParser fieldSpec "" t
+        `parseSatisfies` \FieldSpec{..} -> fieldMod == Repeated
+
+    it "parses any fieldspec without repeated as Optional" $ property $
+      forAll optionalFieldSpec $ \t -> runParser fieldSpec "" t
+        `parseSatisfies` \FieldSpec{..} -> fieldMod == Optional
+
+    it "parses the type of a fieldspec correctly" $ property $
+      forAll (liftA2 (,) anyFieldSpec anyTypeString) $ \(spec, (pt, tt)) -> do
+        spec' <- case (T.words spec) of
+          [type', ident', eq, tag] ->
+            pure $ T.unwords [T.pack tt, ident', eq, tag]
+          [res, type', ident', eq, tag] ->
+            pure $ T.unwords [res, T.pack tt, ident', eq, tag]
+          _ -> fail "Unexpected number of words in fieldspec"
+
+        runParser fieldSpec "" spec' `parseSatisfies` \FieldSpec{..} ->
+          fieldType == pt
+
+
+    it "parses the name of a fieldspec correctly" $ property $
+      forAll (liftA2 (,) anyFieldSpec anyIdentifier) $ \(spec, name') -> do
+        spec' <- case (T.words spec) of
+          [type', ident', eq, tag] ->
+            pure $ T.unwords [type', T.pack name', eq, tag]
+          [res, type', ident', eq, tag] ->
+            pure $ T.unwords [res, type', T.pack name', eq, tag]
+          _ -> fail "Unexpected number of words in fieldspec"
+
+        runParser fieldSpec "" spec' `parseSatisfies` \FieldSpec{..} ->
+          fieldName == name'
+
+
+    it "parses the tag of a fieldspec correctly" $ property $
+      forAll anyFieldSpec $ \spec (Positive tag) -> do
+        let tag' = T.pack $ show (tag :: Int)
+        spec' <- case (T.words spec) of
+          [type', ident', eq, tag] ->
+            pure $ T.unwords [type', ident', eq, tag' <> ";"]
+          [res, type', ident', eq, tag] ->
+            pure $ T.unwords [res, type', ident', eq, tag' <> ";"]
+          _ -> fail "Unexpected number of words in fieldspec"
+
+        runParser fieldSpec "" spec' `parseSatisfies` \FieldSpec{..} ->
+          fieldTag == tag
+
+
+
+specBodyEntryTests :: Spec
+specBodyEntryTests =
+  describe "specBodyEntry" $ do
+    pure ()
+
+protoSpecBodyTests :: Spec
+protoSpecBodyTests =
+  describe "protoSpecBody" $ do
+    pure ()
+
+protoSpecTests :: Spec
+protoSpecTests =
+  describe "protoSpec" $ do
+    pure ()
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,7 @@
 module Main(main) where
 
-import Test.Hspec
-import Data.Protobuf.Parser.Tests
+import           Data.Protobuf.Parser.Tests
+import           Test.Hspec
 
 main :: IO ()
 main = hspec $ do
