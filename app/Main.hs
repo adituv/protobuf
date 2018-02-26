d1ebb519c3431e59d30b3b48024da9f333dd9292
Iris Ward <aditu.venyhandottir@gmail.com>
2018-02-26 17:20:23 +0000

    Hacky prototype program implementation

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Data.Protobuf.CodeGen
+import Data.Protobuf.Encoding
+import Data.Protobuf.Parser
+
+import qualified Data.Text.Lazy
+import qualified Data.Text.IO as TIO
+import qualified Data.Text.Lazy.IO as TLIO
+import           System.Environment(getArgs)
+import           System.Exit(exitFailure)
+import           Text.Megaparsec(parseErrorPretty, runParser)
+
+type LazyText = Data.Text.Lazy.Text
+
 main :: IO ()
-main = putStrLn "Blah" -- To allow compiling the whole project
+main = do
+    (inFileName:_) <- getArgs
+    src <- TIO.readFile inFileName
+    let scope = "Autogen"
+    let maybeSpec = runParser proto3File inFileName src
+    spec <- case maybeSpec of
+      Right spec -> pure spec
+      Left perror -> do
+          putStrLn $ parseErrorPretty perror
+          exitFailure
+    let files = genFiles scope spec
+    mapM_ (uncurry TLIO.writeFile) files
+
