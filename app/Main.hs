de905566a9e5b6b93d2a76832047f05398a821a2
Iris Ward <aditu.venyhandottir@gmail.com>
2018-03-03 13:46:17 +0000

    Implement -o flag, allow directory creation

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,43 @@
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE NamedFieldPuns    #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 module Main where
 
-import Data.Protobuf.CodeGen
-import Data.Protobuf.Encoding
-import Data.Protobuf.Parser
+import           Options
 
-import qualified Data.Text.Lazy
-import qualified Data.Text.IO as TIO
-import qualified Data.Text.Lazy.IO as TLIO
-import           System.Environment(getArgs)
-import           System.Exit(exitFailure)
-import           Text.Megaparsec(parseErrorPretty, runParser)
+import           Data.Protobuf.CodeGen
+import           Data.Protobuf.Parser
 
-type LazyText = Data.Text.Lazy.Text
+import           Control.Monad         (forM_)
+import           Data.Semigroup        ((<>))
+import qualified Data.Text.IO          as TIO
+import qualified Data.Text.Lazy.IO     as TLIO
+import           Options.Applicative   (execParser)
+import           System.Exit           (exitFailure)
+import           System.Directory      (createDirectoryIfMissing)
+import           System.FilePath       (takeDirectory, (</>))
+import           Text.Megaparsec       (parseErrorPretty, runParser)
 
 main :: IO ()
 main = do
-    (inFileName:_) <- getArgs
-    src <- TIO.readFile inFileName
-    let scope = "Autogen"
-    let maybeSpec = runParser proto3File inFileName src
-    spec <- case maybeSpec of
-      Right spec -> pure spec
-      Left perror -> do
-          putStrLn $ parseErrorPretty perror
-          exitFailure
-    let files = genFiles scope spec
-    mapM_ (uncurry TLIO.writeFile) files
+    opts <- execParser options
+    compileProtos opts
 
+compileProtos :: Options -> IO ()
+compileProtos opts@Options{sourceFiles} = mapM_ (compileProto opts) sourceFiles
+
+compileProto :: Options -> FilePath -> IO ()
+compileProto Options{..} srcPath = do
+    src <- TIO.readFile srcPath
+    spec <- pure (runParser proto3File srcPath src) >>= \case
+        Left perror -> do
+            putStrLn $ "Compilation error in \"" <> srcPath <> "\":\n"
+            putStrLn $ "\t" <> parseErrorPretty perror
+            exitFailure
+        Right s -> pure s
+    let dstFiles = genFiles namespace spec
+    forM_ dstFiles $ \(path, contents) -> do
+        let dir = outdir </> takeDirectory path
+        createDirectoryIfMissing True dir   -- True = create parents
+        TLIO.writeFile (outdir </> path) contents
