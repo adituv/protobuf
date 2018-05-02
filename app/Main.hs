dcff9dc8d628cd3d99ed9767ba53fe7203218506
Iris Ward <aditu.venyhandottir@gmail.com>
2018-05-02 23:54:42 +0100

    BROKEN - now compiles but name resolution fails

--- app/Main.hs
+++ app/Main.hs
@@ -32,11 +32,14 @@ compileProto Options{..} srcPath = do
     src <- TIO.readFile srcPath
     spec <- pure (runParser proto3File srcPath src) >>= \case
         Left perror -> do
-            putStrLn $ "Compilation error in \"" <> srcPath <> "\":\n"
+            putStrLn $ "Compilation error in \"" <> srcPath <> "\":"
+            putStrLn ""
             putStrLn $ "\t" <> parseErrorPretty perror
             exitFailure
         Right s -> pure s
-    let dstFiles = genFiles namespace spec
+    dstFiles <- case genFiles namespace spec of
+        Just files -> pure files
+        Nothing    -> putStrLn "Codegen error!!!" >> exitFailure
     forM_ dstFiles $ \(path, contents) -> do
         let dir = outdir </> takeDirectory path
         createDirectoryIfMissing True dir   -- True = create parents
--- src/Data/Protobuf/CodeGen.hs
+++ src/Data/Protobuf/CodeGen.hs
@@ -7,6 +7,7 @@ import           Data.Protobuf.ProtoSpec
 import           Control.Arrow           ((&&&))
 import           Data.Char               (toUpper)
 import qualified Data.HashMap.Lazy       as HashMap
+import qualified Data.List               as List
 import           Data.Monoid             (mconcat, (<>))
 import           Data.String             (IsString (..))
 import qualified Data.Text.Lazy          as Text.Lazy
@@ -17,8 +18,8 @@ import qualified Data.Tree.ScopeTree     as ScopeTree
 type LazyText = Text.Lazy.Text
 type TextBuilder = Text.Builder.Builder
 
-genFiles :: LazyText -> ProtoSpec -> [(FilePath, LazyText)]
-genFiles scope spec = HashMap.toList $ genFilesAux scope' spec
+genFiles :: LazyText -> ProtoSpec -> Maybe [(FilePath, LazyText)]
+genFiles scope spec = HashMap.toList <$> genFilesAux scope' spec
   where
     scope' = Text.Lazy.intercalate "."
            . fmap capitalizeT
@@ -30,18 +31,21 @@ genFiles scope spec = HashMap.toList $ genFilesAux scope' spec
       Nothing    -> ""
       Just (h,t) -> Text.Lazy.cons (toUpper h) t
 
-genFilesAux :: LazyText -> ProtoSpec -> HashMap.HashMap FilePath LazyText
-genFilesAux scope spec@ProtoSpec{..} =
-    HashMap.insert fileName (genFile scope spec)
-  $ HashMap.unions (genFilesAux innerScope <$> innerSpecs)
+genFilesAux :: LazyText -> ProtoSpec -> Maybe (HashMap.HashMap FilePath LazyText)
+genFilesAux scope spec@ProtoSpec{..} = do
+    childFiles <- mapM (genFilesAux innerScope) innerSpecs
+    thisFile <- genFile scope spec
+    pure $ HashMap.insert fileName thisFile (HashMap.unions childFiles)
   where
     innerScope = Text.Builder.toLazyText
                $ addScope scope (fromString $ capitalize messageName)
     fileName = Text.Lazy.unpack $
                Text.Lazy.replace "." "/" innerScope <> ".hs"
 
-genFile :: LazyText -> ProtoSpec -> LazyText
-genFile scope spec = Text.Builder.toLazyText . genSpec scope (collectNames spec) $ spec
+genFile :: LazyText -> ProtoSpec -> Maybe LazyText
+genFile scope spec = Text.Builder.toLazyText <$> genSpec scope (collectNames currentScope spec) spec
+  where
+    currentScope = Text.Lazy.splitOn "." scope
 
 collectNames :: [LazyText] -> ProtoSpec -> ScopeTree LazyText
 collectNames scope ProtoSpec{..} = ScopeTree.insert messageScoped
@@ -52,24 +56,26 @@ collectNames scope ProtoSpec{..} = ScopeTree.insert messageScoped
 endl :: TextBuilder
 endl = "\n"
 
-genSpec :: LazyText -> ScopeTree LazyText -> ProtoSpec -> TextBuilder
-genSpec scope namesInScope ProtoSpec{..} =
-       "{-# LANGUAGE RecordWildCards #-}" <> endl
-    <> "module " <> scopedName <> "(module Data.Protobuf, module " <> scopedName <> ") where" <> endl
-    <> endl
-    <> "import Data.Protobuf" <> endl
-    <> "import Data.Protobuf.Encoding" <> endl
-    <> "import GHC.Generics(Generic)" <> endl
-    <> mconcat (genImport scopedName <$> innerSpecs)
-    <> endl
-    <> "data " <> datatypeName <> " = " <> datatypeName <> endl
-    <> genFields namesInScope fields
-    <> " deriving (Show, Generic)" <> endl <> endl
-    <> genInstances scopedName fields
-    <> endl
+genSpec :: LazyText -> ScopeTree LazyText -> ProtoSpec -> Maybe TextBuilder
+genSpec scope namesInScope ProtoSpec{..} = do
+  fieldDefs <- genFields currentScope namesInScope fields
+  pure $ "{-# LANGUAGE RecordWildCards #-}" <> endl
+      <> "module " <> scopedName <> "(module Data.Protobuf, module " <> scopedName <> ") where" <> endl
+      <> endl
+      <> "import Data.Protobuf" <> endl
+      <> "import Data.Protobuf.Encoding" <> endl
+      <> "import GHC.Generics(Generic)" <> endl
+      <> mconcat (genImport scopedName <$> innerSpecs)
+      <> endl
+      <> "data " <> datatypeName <> " = " <> datatypeName <> endl
+      <> fieldDefs
+      <> " deriving (Show, Generic)" <> endl <> endl
+      <> genInstances scopedName fields
+      <> endl
   where
     datatypeName = fromString $ capitalize messageName
     scopedName = addScope scope datatypeName
+    currentScope = Text.Lazy.splitOn "." scope
 
 addScope :: LazyText -> TextBuilder -> TextBuilder
 addScope scope name
@@ -104,49 +110,65 @@ genInstances scopedName fields =
       "  toRawValue msg = RLengthEncoded $ runPut putRawMessage (toProto msg)" <> endl <>
       "  fromRawValue (RLengthEncoded raw) = error \"TODO\""
 
-
 genFieldFromProto :: FieldSpec -> TextBuilder
 genFieldFromProto FieldSpec{..} = "raw .: " <> fromString (show fieldTag)
 
 genFieldToProto :: FieldSpec -> TextBuilder
 genFieldToProto FieldSpec{..} = "      & Map.insert " <> fromString (show fieldTag) <> "(toRaw " <> fromString fieldName <> ")" <> endl
 
-genFields :: [FieldSpec] -> ScopeTree LazyText -> TextBuilder
-genFields fields namesInScope =
-  "  { " <> intercalate (endl <> "  , ") (genField namesInScope <$> fields)
-         <> endl <> "  }"
+genFields :: [LazyText] -> ScopeTree LazyText -> [FieldSpec] -> Maybe TextBuilder
+genFields currentScope namesInScope fields= do
+  fieldSpecs <- mapM (genField currentScope namesInScope) fields
+  pure $ "  { " <> intercalate (endl <> "  , ") fieldSpecs <> endl <> "  }"
 
-genField :: FieldSpec -> ScopeTree LazyText -> TextBuilder
-genField FieldSpec{..} namesInScope =
-    fromString fieldName <> " :: " <> fieldType'
-  where
-    fieldType' = case fieldMod of
-      Optional -> genType namesInScope fieldType
-      Repeated -> "[" <> genType nameInScope fieldType <> "]"
+genField :: [LazyText] -> ScopeTree LazyText -> FieldSpec -> Maybe TextBuilder
+genField currentScope namesInScope FieldSpec{..} = do
+  fieldType' <- case fieldMod of
+    Optional -> genType currentScope namesInScope fieldType
+    Repeated -> genType currentScope namesInScope fieldType >>= \x -> pure ("[" <> x <> "]")
+  pure $ fromString fieldName <> " :: " <> fieldType'
 
 genType :: [LazyText] -> ScopeTree LazyText -> ProtoType -> Maybe TextBuilder
-genType _ _ PDouble        = Just "Double"
-genType _ _ PFloat         = Just "Float"
-genType _ _ PInt32         = Just "Int32"
-genType _ _ PInt64         = Just "Int64"
-genType _ _ PUInt32        = Just "Word32"
-genType _ _ PUInt64        = Just "Word64"
-genType _ _ PSInt32        = Just "Int32"
-genType _ _ PSInt64        = Just "Int64"
-genType _ _ PFixed32       = Just "Word32"
-genType _ _ PFixed64       = Just "Word64"
-genType _ _ PSFixed32      = Just "Int32"
-genType _ _ PSFixed64      = Just "Int64"
-genType _ _ PBool          = Just "Bool"
-genType _ _ PString        = Just "Text"
-genType _ _ PBytes         = Just "ByteString"
+genType _ _ PDouble         = Just "Double"
+genType _ _ PFloat          = Just "Float"
+genType _ _ PInt32          = Just "Int32"
+genType _ _ PInt64          = Just "Int64"
+genType _ _ PUInt32         = Just "Word32"
+genType _ _ PUInt64         = Just "Word64"
+genType _ _ PSInt32         = Just "Int32"
+genType _ _ PSInt64         = Just "Int64"
+genType _ _ PFixed32        = Just "Word32"
+genType _ _ PFixed64        = Just "Word64"
+genType _ _ PSFixed32       = Just "Int32"
+genType _ _ PSFixed64       = Just "Int64"
+genType _ _ PBool           = Just "Bool"
+genType _ _ PString         = Just "Text"
+genType _ _ PBytes          = Just "ByteString"
 genType scope names (NamedField s)
-  | s `startsWith` "."     = Just $ normalizeType (tail s)
-  | otherwise              = resolveName s
+  | "." `List.isPrefixOf` s = resolveNameAbsolute (tail s)
+  | otherwise               = resolveName s
   where
-    possibleScopes = reverse $ inits scope
-    possibleScopedNames = fmap (++ [name]) possibleScopes
-    resolveName name = find (ScopeTree.contains names) possibleScopedNames
+    possibleScopes :: [[LazyText]]
+    possibleScopes = reverse $ List.inits scope
+
+    resolveName :: String -> Maybe TextBuilder
+    resolveName name = fmap normalizeType
+                     . List.find (ScopeTree.contains names)
+                     . fmap ( flip (++)
+                            . Text.Lazy.splitOn "."
+                            . fromString
+                            $ name
+                            )
+                     $ possibleScopes
+
+    resolveNameAbsolute :: String -> Maybe TextBuilder
+    resolveNameAbsolute name
+      | ScopeTree.contains names (Text.Lazy.splitOn "." $ fromString name)
+          = Just $ normalizeType (Text.Lazy.splitOn "." $ fromString name)
+      | otherwise = Nothing
+
+    normalizeType :: [LazyText] -> TextBuilder
+    normalizeType = intercalate "." . fmap (Text.Builder.fromLazyText . capitalizeText)
 
 intercalate :: TextBuilder -> [TextBuilder] -> TextBuilder
 intercalate _   []     = mempty
@@ -156,3 +178,8 @@ intercalate mid (x:xs) = x <> mid <> intercalate mid xs
 capitalize :: String -> String
 capitalize []     = []
 capitalize (x:xs) = toUpper x : xs
+
+capitalizeText :: LazyText -> LazyText
+capitalizeText txt = case Text.Lazy.uncons txt of
+    Just (h,t) -> Text.Lazy.cons (toUpper h) t
+    Nothing    -> txt
