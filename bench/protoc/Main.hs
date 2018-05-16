cfe12c5e16371b5b61f8434c113034dad3f2bd75
Iris Ward <aditu.venyhandottir@gmail.com>
2018-05-16 15:21:30 +0100

    Benchmark compilers

--- bench/protoc/Main.hs
+++ bench/protoc/Main.hs
@@ -1,6 +1,14 @@
 module Main(main) where
 
-{-
+import Criterion.Main
+
+import qualified Data.List as List
+import Data.Monoid((<>))
+import System.Directory(createDirectoryIfMissing, removeDirectoryRecursive)
+import System.Info(os, arch)
+import System.Process
+import System.Exit(ExitCode(..))
+
 protocZipInfo :: (String, String)
 protocZipInfo =
   let
@@ -16,14 +24,12 @@ protocZipInfo =
   in
     (os', arch')
 
-protocZipFilePath :: FilePath
-protocZipFilePath = concat ["protoc-3.3.0-", os', "-", arch', ".zip"]
+protocZipUrl :: FilePath
+protocZipUrl =
+    "https://github.com/google/protobuf/releases/download/v3.3.0/protoc-3.3.0-"
+    <> os' <> "-" <> arch' <> ".zip"
   where
     (os', arch') = protocZipInfo
-
-protocZipUrl :: FilePath
-protocZipUrl = "https://github.com/google/protobuf/releases/download/v3.3.0/"
-               <> protocZipFilePath
 
 installProtoc :: IO FilePath
 installProtoc = do
@@ -31,23 +37,70 @@ installProtoc = do
     case code of
         ExitSuccess -> do
           putStrLn $ "protoc found: " <> protocPath
-          pure protocPath
+          pure $ trimEndLines protocPath
         ExitFailure _ -> do
           putStrLn "protoc not found.  Installing to /tmp/bin/protoc"
           fetchAndInstall
           pure "/tmp/bin/protoc"
   where
     fetchAndInstall = do
-      callProcess "curl" ["-OL", protocZipUrl]
-      callProcess "unzip" ["-o", protocZipFilePath, "-d", "/tmp", "bin/protoc"]
+      callProcess "curl" ["-o", "/tmp/protoc.zip", "-L", protocZipUrl]
+      callProcess "unzip" ["-o", "/tmp/protoc.zip", "-d", "/tmp", "bin/protoc"]
+
+getProtoPluginPath :: IO FilePath
+getProtoPluginPath = do
+  protoPluginPath <- readProcess "which" ["proto-lens-protoc"] ""
+  pure $ trimEndLines protoPluginPath
+
+getHsProtocPath :: IO FilePath
+getHsProtocPath = do
+  hsProtocPath <- readProcess "which" ["hsprotoc"] ""
+  pure $ trimEndLines hsProtocPath
+
+trimEndLines :: String -> String
+trimEndLines = List.dropWhileEnd (=='\n')
+
+benchProtoc :: FilePath -> FilePath -> FilePath -> Benchmark
+benchProtoc protocPath pluginPath protoPath =
+  bench protoPath $ perRunEnvWithCleanup
+    (createDirectoryIfMissing True "/tmp/protoc-out")
+    (\_ -> removeDirectoryRecursive "/tmp/protoc-out")
+    (\_ -> runProtoc
+             protocPath
+             pluginPath
+             ("bench/" <> protoPath <> ".proto")
+    )
+
+
+runProtoc :: FilePath -> FilePath -> FilePath -> IO ()
+runProtoc protocPath pluginPath protoPath =
+  callProcess protocPath
+    [ "--plugin"
+    , "protoc-gen-haskell=" <> pluginPath
+    , "--haskell_out"
+    , "/tmp/protoc-out"
+    , protoPath
+    ]
+
+benchHsProtoc :: FilePath -> FilePath -> Benchmark
+benchHsProtoc hsprotocPath protoPath =
+  bench protoPath $ perRunEnvWithCleanup
+    (createDirectoryIfMissing True "/tmp/hsprotoc-out")
+    (\_ -> removeDirectoryRecursive "/tmp/hsprotoc-out")
+    (\_ -> callProcess hsprotocPath
+             [ "-o", "/tmp/hsprotoc-out"
+             , "bench/" <> protoPath <> ".proto"
+             ]
+    )
 
 main :: IO ()
-main = defaultMain
-  [ bgroup "Round trip encoding+decoding"
-      bgroup "SearchResponse"
-        [ roundTrip sampleMessage sampleMessageBytes ]
-  ]
--}
-
-main :: IO ()
-main = pure ()
+main = do
+  protocPath <- installProtoc
+  protoPluginPath <- getProtoPluginPath
+  hsProtocPath <- getHsProtocPath
+  defaultMain
+    [ bgroup "protoc"
+       [ benchProtoc protocPath protoPluginPath "SearchResponse" ]
+    , bgroup "hsprotoc"
+       [ benchHsProtoc hsProtocPath "SearchResponse" ]
+    ]
